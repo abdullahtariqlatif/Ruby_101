9a406955b9bfacc981ab16891b84c817a46dde2e
Abdullah Tariq <abdullahtariqlatif@gmail.com>
2021-06-20 20:40:31 +0500

    A simple Mad Lib Game

---
+++ Practice_8_Mad_Libs_Game/Mad_Lib_Poem_Game.rb
@@ -0,0 +1,14 @@
+puts"Enter a color: "
+color_name1 = gets.chomp()
+
+puts"Enter a plural noun: "
+plural_noun1 = gets.chomp()
+
+puts"Enter a name: "
+person_name = gets.chomp()
+
+
+puts("Roses are + "color_name1)
+puts plural_noun1 +" are blue"
+puts "I love {person_name}"
+puts"\n"
